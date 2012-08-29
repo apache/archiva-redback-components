ca925b189faaa5e93281c0b70c1e14952b02bef4
Olivier Lamy <olamy@apache.org>
2012-08-29 13:37:58 +0000

    fix site deploy script
    git-svn-id: https://svn.apache.org/repos/asf/archiva/redback/redback-components/trunk@1378548 13f79535-47bb-0310-9956-ffa450edef68

--- deploySite.sh
+++ deploySite.sh
@@ -1,1 +1,1 @@
-mvn site-deploy &&  mvn scm-publish:publish-scm  -f redback-components-parent/pom.xml 
+mvn clean site-deploy -f redback-components-parent/pom.xml -Pall  &&  mvn scm-publish:publish-scm  -f redback-components-parent/pom.xml 
